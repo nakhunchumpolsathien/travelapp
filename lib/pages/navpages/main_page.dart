8e03712578ae6179e9a6c1e2fc87f06737e01646
Nakhun Chumpolsathien <45205446+nakhunchumpolsathien@users.noreply.github.com>
2022-04-20 15:30:49 +0700

    Initial commit

---
+++ lib/pages/navpages/main_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:travelapp/pages/navpages/bar_item_page.dart';
+import 'package:travelapp/pages/navpages/home_page.dart';
+import 'package:travelapp/pages/navpages/my_page.dart';
+import 'package:travelapp/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    HomePage(),
+    BarItemPage(),
+    SearchPage(),
+    MyPage()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[0],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false
+        items: [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.apps)),
+          BottomNavigationBarItem(label: 'Bar', icon: Icon(Icons.bar_chart_sharp)),
+          BottomNavigationBarItem(label: 'Search', icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: 'My', icon: Icon(Icons.person))
+        ],
+      ),
+    );
+  }
+}
+
